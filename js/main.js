c656700a661712dfd97aff5c8b58c597baae7767
Emorinken <emorinkensam@gmail.com>
2022-06-19 18:08:24 -0700

    firs commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const h1= document.querySelector("h1")
+const first= document.querySelector("#first")
+const second= document.querySelector("#second")
+const third= document.querySelector("#third")
+const btn= document.querySelector("button")
+const input= document.querySelector("input")
+
+const func=()=>{
+
+
+async function getApi(){
+	const res= await fetch(`https://city-names-api.herokuapp.com/api/${input.value}`)
+	const data = await res.json()
+	first.textContent=data.name
+	second.textContent=data.age
+	third.textContent=data.nationality
+	console.log(data)
+}
+getApi()
+}
+btn.addEventListener("click",func)
+// fetch("https://city-names-api.herokuapp.com/api")
+// 	.then(res=>res.json())
+// 	.then(data=>{
+// 		console.log(data)
+// 	})
+// 	.catch(err=>{
+// 		console.log(err)
+// 	})
