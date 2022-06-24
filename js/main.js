459564e42f5e73cb6b3bd8854b61d856810418ef
Emorinken <emorinkensam@gmail.com>
2022-06-24 02:26:54 -0700

    added the enter key event listener

--- js/main.js
+++ js/main.js
@@ -19,6 +19,16 @@ async function getApi(){
 getApi()
 }
 btn.addEventListener("click",func)
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+	// If the user presses the "Enter" key on the keyboard
+	if (event.key === "Enter") {
+	  // Cancel the default action, if needed
+	  event.preventDefault();
+	  // Trigger the button element with a click
+	  btn.click();
+	}
+  });
 // fetch("https://city-names-api.herokuapp.com/api")
 // 	.then(res=>res.json())
 // 	.then(data=>{
